fe6e5c90ebfc3023bbf51038980e579039941713
anonymous <jcolivati@gmail.com>
2023-09-23 08:12:30 -0300

    fix: change handler

--- src/main/java/br/com/course/strkafkaconsumer/annotation/StrConsumerCustomListener.java
+++ src/main/java/br/com/course/strkafkaconsumer/annotation/StrConsumerCustomListener.java
@@ -13,11 +13,14 @@ import java.lang.annotation.Target;
 @KafkaListener
 public @interface StrConsumerCustomListener {
 
-    @AliasFor(annotation = KafkaListener.class, value = "groupId")
+    @AliasFor(annotation = KafkaListener.class, attribute = "groupId")
     String groupId() default "";
-    @AliasFor(annotation = KafkaListener.class, value = "topics")
+    @AliasFor(annotation = KafkaListener.class, attribute = "topics")
     String[] topics() default "str-topic";
 
-    @AliasFor(annotation = KafkaListener.class, value = "containerFactory")
+    @AliasFor(annotation = KafkaListener.class, attribute = "containerFactory")
     String containerFactory() default "strContainerFactory";
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "errorHandler")
+    String errorHandler() default "errorCustomHandler";
 }
