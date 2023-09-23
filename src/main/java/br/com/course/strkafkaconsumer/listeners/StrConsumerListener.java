6ab6cb646f7c8299aa588adb150dc2b8bf0316c0
anonymous <jcolivati@gmail.com>
2023-09-23 07:56:24 -0300

    feat: created handler

--- src/main/java/br/com/course/strkafkaconsumer/listeners/StrConsumerListener.java
+++ src/main/java/br/com/course/strkafkaconsumer/listeners/StrConsumerListener.java
@@ -1,16 +1,29 @@
 package br.com.course.strkafkaconsumer.listeners;
 
+import br.com.course.strkafkaconsumer.annotation.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listner(String message){
-        log.info("Recive Message {}", message);
+    @SneakyThrows
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message){
+        log.info("CREATE ::: Recive Message {}", message);
+        throw new IllegalArgumentException("Exception");
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message){
+        log.info("LOG ::: Recive Message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2", containerFactory = "validContainerFactory")
+    public void history(String message){
+        log.info("HISTORY ::: Recive Message {}", message);
     }
 
 }
